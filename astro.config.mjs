82af6799c23f5d27b0356aea6fddf0af8965b65b
andreabr4 <br13.andrea@gmail.com>
2024-02-27 20:28:55 +0100

    first attempt to implement translations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,20 @@
 import { defineConfig } from 'astro/config';
-
+import astroI18next from "astro-i18next";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  // experimental: {
+  //   integrations: true,
+  // },
+  integrations: [
+    tailwind(),
+    // astroI18next({
+    //   baseLanguage: "es",
+    //   i18next: {
+    //     debug: true,
+    //     supportedLngs: ["en", "es"],
+    //   }
+    // })
+  ]
 });
