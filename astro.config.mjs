702e08b0b11fea6ac144e0a3e33534293a1030b3
andreabr4 <br13.andrea@gmail.com>
2024-03-03 14:31:00 +0100

    delete vercel statics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import astroI18next from "astro-i18next";
 import tailwind from "@astrojs/tailwind";
-import vercelStatic from '@astrojs/vercel/static';
 import vercel from '@astro/vercel/serverless';
 
 // https://astro.build/config
