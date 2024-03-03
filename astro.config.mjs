9b6fd32c198bc12ec66414e61c680d0e2408f684
andreabr4 <br13.andrea@gmail.com>
2024-03-03 14:37:26 +0100

    fix vercel import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import astroI18next from "astro-i18next";
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astro/vercel/serverless';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
